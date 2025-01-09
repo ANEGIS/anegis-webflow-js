df328a4fbb379c1ab2cb164efbf77d09540a451a
Dariusz Sułtanowski <sultanowski.d@gmail.com>
2025-01-09 10:12:01 +0100

    forms: Unlink forms from index file

--- bin/build.js
+++ bin/build.js
@@ -7,7 +7,12 @@ const BUILD_DIRECTORY = 'dist';
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 // Config entrypoint files
-const ENTRY_POINTS = ['src/index.ts'];
+const ENTRY_POINTS = [
+  'src/index.ts',
+  'src/forms/form-capture-contact.ts',
+  'src/forms/form-capture-knowledge-base.ts',
+  'src/forms/form-capture-offer-contact.ts',
+];
 
 // Config dev serving
 const LIVE_RELOAD = !PRODUCTION;
